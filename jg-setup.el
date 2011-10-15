e1a196d136b3d73358795e80d8a88c8c0fd31d47
Jeff Gran <jeff.gran@openlogic.com>
2011-10-14 23:18:55 -0600

    switch to enhanced-ruby-mode, maximize frame at startup

--- jg-setup.el
+++ jg-setup.el
@@ -1,4 +1,7 @@
 ; custom setup stuff.
+(require 'maxframe)
+(add-hook 'window-setup-hook 'maximize-frame t)
+
 (setq
    backup-by-copying t                             ; don't clobber symlinks
    backup-directory-alist '(("." . "~/.saves"))    ; don't litter my fs tree
