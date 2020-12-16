b9e43210e74408de37f6225f00552474ceba4a7b
Jeff Gran <jgran@doximity.com>
2020-12-15 19:58:57 -0700

    add some go-mode stuff

--- jg/jg-modes.el
+++ jg/jg-modes.el
@@ -280,6 +280,40 @@
 (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
 
 
+
+
+
+
+(defun my-go-mode-hook ()
+  (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
+  (setq gofmt-command "goimports")                ; gofmt uses invokes goimports
+  ;; (if (not (string-match "go" compile-command))   ; set compile command default
+  ;;     (set (make-local-variable 'compile-command)
+  ;;          "go build -v && go test -v && go vet"))
+
+  ;;(flycheck-mode)
+
+  (local-set-key (kbd "A-M-.") 'lsp-goto-type-definition)
+  ;; Key bindings specific to go-mode
+  ;; (local-set-key (kbd "M-.") 'godef-jump)         ; Go to definition
+  ;; (local-set-key (kbd "M-*") 'pop-tag-mark)       ; Return from whence you came
+  ;; (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
+  ;; (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
+  ;; (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
+  ;; (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
+
+)                         ; Enable auto-complete mode
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+(add-hook 'go-mode-hook 'lsp-deferred)
+
+
+
+
+
+
+
+
+
 ;; make comments automatically go to multiple lines for long ones
 (auto-fill-mode t)
 (setq comment-auto-fill-only-comments t)
@@ -401,7 +435,8 @@
 
 
 (when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH"))
 
 
 
